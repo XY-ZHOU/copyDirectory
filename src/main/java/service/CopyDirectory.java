dccceb70d291ce274cd5d66279dd664efcba6130
XY-ZHOU <731986251@qq.com>
2019-02-28 20:36:18 +0800

    add function of copying directory

--- src/main/java/service/CopyDirectory.java
+++ src/main/java/service/CopyDirectory.java
@@ -1,5 +1,23 @@
 package main.java.service;
 
-public class CopyDirectory {
+import java.io.File;
+import java.io.IOException;
 
+public class CopyDirectory {
+    private static void copyDir(String oldPath, String newPath) throws IOException {
+        File oldFile = new File(oldPath);
+        File[] files = oldFile.listFiles();
+        File newFile = new File(newPath);
+        if (!newFile.exists()) {
+            newFile.mkdirs();
+        }
+        for (File file : files) {
+            if (file.isFile()) {
+                copyFile(file.getPath(), newPath + "\\" + file.getName());
+            } else if (file.isDirectory()) {
+                copyDir(file.getPath(), newPath + "\\" + file.getName());
+            }
+        }
+
+    }
 }
